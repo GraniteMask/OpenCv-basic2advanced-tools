573ee4efa491949061811c83f84838f8020126c6
GraniteMask <ratnadeepastronomy@gmail.com>
2021-07-22 02:49:09 +0530

    image and video read

--- read.py
+++ read.py
@@ -1,11 +1,21 @@
 import cv2 as cv
 
-#img = cv.imread('photos/big-bang.jpg') #for reading images
+img = cv.imread('../photos/big-bang.jpg') #for reading images
 
-#cv.imshow('Bang', img)
+cv.imshow('Bang', img)
 
 #Reading Videos
-#capture = cv.VideoCapture(0) #here 0 refers to webcam and number can be increased if more number of webscams is attached to system and you want to choose specific cam
-capture = cv.VideoCapture('')
+capture = cv.VideoCapture(0) #here 0 refers to webcam and number can be increased if more number of webscams is attached to system and you want to choose specific cam
+capture = cv.VideoCapture('../videos/sample.mp4')
+
+while True:
+    isTrue, frame = capture.read() #reading video frame by frame
+    cv.imshow('Video',frame) 
+
+    if cv.waitKey(20) & 0xFF==ord('d'):   #if letter d is press then break out of while loop
+        break
+
+capture.release()
+cv.destroyWindow()
 
 cv.waitKey(0)
