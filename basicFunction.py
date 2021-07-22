fa70454c7a704f046fb403cc2c4981db951ddc84
GraniteMask <ratnadeepastronomy@gmail.com>
2021-07-22 13:13:10 +0530

    blur an image

--- basicFunction.py
+++ basicFunction.py
@@ -8,5 +8,7 @@ gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow('Gray',gray)
 
 # 2) Blur an image
+blur = cv.GaussianBlur(img,(3,3),cv.BORDER_DEFAULT)     #(3,3) is color size and must be an odd number
+cv.imshow('Blur',blur)
 
 cv.waitKey(0)
