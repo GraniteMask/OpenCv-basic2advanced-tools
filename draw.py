cb6a0f4221aa8c9d7bd2e818d97636665d4e7b2b
GraniteMask <ratnadeepastronomy@gmail.com>
2021-07-22 13:00:43 +0530

    adding text

--- draw.py
+++ draw.py
@@ -4,21 +4,21 @@ import numpy as np
 blank = np.zeros((500,500,3), dtype='uint8') #3 for number of color channels
 cv.imshow('Blank',blank)
 
-# img = cv.imread('../photos/big-bang.jpg')
-# cv.imshow('Cat',img)
+img = cv.imread('../photos/big-bang.jpg')
+cv.imshow('Cat',img)
 
 
 # 1) Paint the image of a certain colour
 
-# blank[:] = 0,255,0
-# blank[200:300, 300:400] = 0,255,0
-# cv.imshow('Green',blank)
+blank[:] = 0,255,0
+blank[200:300, 300:400] = 0,255,0
+cv.imshow('Green',blank)
 
 # 2) Drawing a rectangle
 
 cv.rectangle(blank, (0,0), (250,250), (0,255,0), thickness=2)
-# cv.rectangle(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (0,255,0), thickness=2)
-# cv.rectangle(blank, (0,0), (250,250), (0,255,0), thickness=cv.FILLED) #to fill the rectangle area
+cv.rectangle(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (0,255,0), thickness=2)
+cv.rectangle(blank, (0,0), (250,250), (0,255,0), thickness=cv.FILLED) #to fill the rectangle area
 cv.imshow('Rectangle',blank)
 
 # 3) Draw a circle
@@ -31,5 +31,11 @@ cv.imshow('Circle',blank)
 
 cv.line(blank, (0,0), (blank.shape[1]//2,blank.shape[0]//2), (255,255,255), thickness=3)
 cv.imshow('line',blank)
+
+#5)write text on image
+
+cv.putText(blank, 'Hello', (255,255), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0),2)  #(file,text,position,fontname,scale of text,colour,thickness)
+
+cv.imshow('Text',blank)
 
 cv.waitKey(0)
