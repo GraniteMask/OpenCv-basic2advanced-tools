8aa14a000434f1d3945466f5cba7705cbd9efe95
GraniteMask <ratnadeepastronomy@gmail.com>
2021-07-23 00:59:09 +0530

    matplotlib image read and BGR2RGB

--- colourSpaces.py
+++ colourSpaces.py
@@ -1,8 +1,12 @@
 import cv2 as cv 
 import numpy as np
+import matplotlib.pyplot as plt
 
 img = cv.imread('../photos/big-bang.jpg')
-cv.imshow('Bang',img)
+cv.imshow('Bang',img)  #it reads as BGR image
+
+#plt.imshow(img)  #it reads as RGB image so you will see a inversion of colour
+#plt.show()
 
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow('Gray',gray)
@@ -16,5 +20,11 @@ cv.imshow('HSV',hsv)
 lab = cv.cvtColor(img,cv.COLOR_BGR2LAB)
 cv.imshow('LAB',lab)
 
+# 3) BGR to RGB
+rgb = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+cv.imshow('RGB',rgb)
 
 cv.waitKey(0)
+
+#always convert BGR to RGB before loading to matplotlib
+#if we want to convert from HSV to LAB then we first have to convert it to BGR then that BGR to LAbs
