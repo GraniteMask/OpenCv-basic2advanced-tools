e3f0c02a8b63ae62fe67978f855b54f591ed7016
GraniteMask <ratnadeepastronomy@gmail.com>
2021-07-23 01:27:52 +0530

    color channels on blank image

--- colorChannelsSplitMerge.py
+++ colorChannelsSplitMerge.py
@@ -4,16 +4,30 @@ import numpy as np
 img = cv.imread('../photos/big-bang.jpg')
 cv.imshow('Bang',img)  
 
+blank=np.zeros(img.shape[:2], dtype='uint8')
+cv.imshow('Blank',blank)
+
 b,g,r = cv.split(img)
 
-cv.imshow('Blue',b)
-cv.imshow('Green',g)
-cv.imshow('Red',r)
+blue = cv.merge([b,blank,blank])
+green = cv.merge([blank,g,blank])
+red = cv.merge([blank,blank,r])
+
+cv.imshow('Blue',blue)
+cv.imshow('Green',green)
+cv.imshow('Red',red)
+
+# cv.imshow('Blue',b)
+# cv.imshow('Green',g)
+# cv.imshow('Red',r)
 
 print(img.shape)
 print(b.shape)
 print(g.shape)
 print(r.shape)
+
+merged = cv.merge([b,g,r])
+cv.imshow('Merged Image', merged)
  
 
 cv.waitKey(0)
